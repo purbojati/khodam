f27545fdb1080c084312e01020657127767bc3cd
Adjie <official.purbojati@gmail.com>
2024-06-21 23:34:01 +0700

    first

---
+++ cekkhodamonline/src/khodamNames.js
@@ -0,0 +1,17 @@
+export const names = [
+    "Kenanga", "Kamboja", "Cempaka", "Melati Hitam", "Anggrek Dendam", "Bunga Setan", "Bunga Bangkai", "Bunga Puspa", "Bunga Wijaya Kusuma", "Bunga Arjuna", "Harimau Jawa", "Ayam Cemani", "Burung Hantu", "Biawak Hitam", "Ular Sendok", "Kucing Hitam", "Macan Kumbang", "Lutung Kasarung", "Gagak Hitam", "Jenglot", 
+    "Roro Jonggrang", "Brawijaya", "Ken Arok", "Kebo Iwa", "Gajah Mada", "Ki Ageng Selo", "Sri Sultan Hamengkubuwono", "Raden Wijaya", "Sunan Kalijaga", "Diponegoro",
+    "Keraton", "Candi Prambanan", "Borobudur", "Gunung Lawu", "Parangkusumo", "Pantai Parangtritis", "Sendang Kamal", "Sumur Gumuling", "Lawang Sewu", "Taman Sari",
+    "Warung Doyong", "Pohon Beringin", "Pohon Trembesi", "Pohon Keluwih", "Pohon Randu", "Pohon Gayam", "Pohon Asam", "Pohon Pulus", "Pohon Jati", "Pohon Sawo Kecik",
+    "Mantra Sirep", "Ajian Jaran Goyang", "Ajian Brajamusti", "Ajian Pengasihan", "Ilmu Pelet", "Ajian Lembu Sekilan", "Ajian Rawarontek", "Ajian Pancasona", "Ajian Semar Mesem", "Ajian Teluh",
+    "Semar", "Gareng", "Petruk", "Bagong", "Arjuna", "Bima", "Nakula", "Sadewa", "Srikandi", "Gatotkaca",
+    "Keris Kyai Setan Kober", "Keris Sangkelat", "Keris Naga Sasra", "Keris Tumbal", "Keris Kiai Carubuk", "Tombak Kyai Pleret", "Panah Arjuna", "Gada Rujak Polo", "Kujang Sakti", "Tombak Kyai Slamet",
+    "Sajen", "Kemenyan", "Keris Pusaka", "Tosan Aji", "Gamelan", "Topeng Panji", "Wayang Kulit", "Wayang Golek", "Wayang Wong", "Jimat Kalimasada",
+    "Gamelan", "Kendang", "Saron", "Bonang", "Gong", "Rebab", "Suling", "Ketipung", "Kenong", "Gender",
+    "Gunung Merapi", "Gunung Merbabu", "Gunung Slamet", "Gunung Kawi", "Gunung Sumbing", "Kraton Surakarta", "Kraton Yogyakarta", "Goa Selarong", "Puncak Gunung Lawu", "Sendang Sono",
+    "Pantai Ngobaran", "Pantai Parangkusumo", "Pantai Baron", "Pantai Siung", "Pantai Krakal", "Pantai Sundak", "Pantai Timang", "Pantai Pok Tunggal", "Pantai Glagah", "Pantai Depok",
+    "Batik", "Kebaya", "Jarik", "Beskap", "Blangkon", "Udeng", "Sarung", "Lurik", "Kain Poleng", "Dodot",
+    "Angklung", "Bedug", "Pencak Silat", "Tari Kecak", "Tari Bedhaya", "Tari Gambyong", "Tari Srimpi", "Pringgitan", "Pendopo", "Balairung",
+
+  ];
+  
